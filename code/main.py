f34518edd0e4c849aec84980a8864bc58629647e
bujor <bujormihaialexandru@gmail.com>
2025-04-10 19:00:31 +0300

    added player and stars

--- code/main.py
+++ code/main.py
@@ -1,22 +1,21 @@
 import pygame
+from random import randint
 
 pygame.init()
 
 WINDOW_WIDTH, WINDOW_HEIGHT = 1080, 600 # depinde de ecran
-
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
-
 pygame.display.set_caption("codemy-space-shooter")
-
 running = True
 
-player_surface = pygame.Surface((100, 200))
-player_surface.fill('orange')
-
-cursor_surface = pygame.Surface((5, 5))
+player_surface = pygame.image.load("images/player.png").convert_alpha()
+star_surface = pygame.image.load("images/star.png").convert_alpha()
 
 x = 100
-# increment = 0.1
+star_count = 20
+
+random_x = [randint(0, WINDOW_WIDTH) for _ in range(star_count)]
+random_y = [randint(0, WINDOW_HEIGHT) for _ in range(star_count)]
 
 # main loop
 while running:
@@ -24,21 +23,16 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        # if event.type == pygame.KEYDOWN:
-        #     if event.key == pygame.K_SPACE:
-        #         if increment == 0:
-        #             increment = 0.1
-        #         else:
-        #             increment = 0
 
     # desenam
     display_surface.fill('steelblue4')
 
-    mouse_x, mouse_y = pygame.mouse.get_pos()
+    for i in range(star_count):
+        display_surface.blit(star_surface, (random_x[i], random_y[i]))
+        
+    display_surface.blit(player_surface, (x, 200))
 
-    # display_surface.blit(player_surface, (x, 200))
-    # x += increment
-    display_surface.blit(cursor_surface, (mouse_x, mouse_y))
+    x += 0.1
 
     pygame.display.update()
 
