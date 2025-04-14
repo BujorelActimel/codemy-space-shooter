f8b773dacd2fabf798d657cfc98566f3a2556016
bujor <bujormihaialexandru@gmail.com>
2025-04-14 19:24:40 +0300

    delta time and vectors

--- code/main.py
+++ code/main.py
@@ -7,6 +7,7 @@ WINDOW_WIDTH, WINDOW_HEIGHT = 1080, 600 # depinde de ecran
 display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption("codemy-space-shooter")
 running = True
+clock = pygame.time.Clock()
 
 player_surface = pygame.image.load("images/player.png").convert_alpha()
 player_rect = player_surface.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2)) 
@@ -16,8 +17,8 @@ meteor_rect = meteor_surface.get_frect(center=(WINDOW_WIDTH/2, WINDOW_HEIGHT/2))
 
 star_surface = pygame.image.load("images/star.png").convert_alpha()
 
-direction = 1
-speed = 0.4
+direction = pygame.math.Vector2(1, -1)
+speed = 500
 
 star_count = 20
 
@@ -26,10 +27,19 @@ random_y = [randint(0, WINDOW_HEIGHT) for _ in range(star_count)]
 
 # main loop
 while running:
+    dt = clock.tick() / 1000
+    # print(clock.get_fps())
+
     # evenimente
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN: # daca cineva a apast o tasta
+            if event.key == pygame.K_1:
+                print(1)
+            if event.key == pygame.K_RETURN:
+                player_rect.center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+            
 
     # desenam
     display_surface.fill(pygame.Color(25, 8, 33))
@@ -40,15 +50,23 @@ while running:
     display_surface.blit(meteor_surface, meteor_rect)
     display_surface.blit(player_surface, player_rect)
 
-    print(player_rect.right)
+    # if player_rect.top < 0:
+    #     player_rect.top = 0
+    #     direction.y *= -1
 
-    # if player_rect.right >= WINDOW_WIDTH or player_rect.left <= 0:
-    #     direction *= -1
+    # if player_rect.bottom > WINDOW_HEIGHT:
+    #     player_rect.bottom = WINDOW_HEIGHT
+    #     direction.y *= -1
     
-    player_rect.x += direction * speed
+    # if player_rect.left < 0:
+    #     player_rect.left = 0
+    #     direction.x *= -1
 
-    if player_rect.x > WINDOW_WIDTH:
-        player_rect.right = 0
+    # if player_rect.right > WINDOW_WIDTH:
+    #     player_rect.right = WINDOW_WIDTH
+    #     direction.x *= -1
+
+    # player_rect.center += direction * speed * dt
 
     pygame.display.update()
 
